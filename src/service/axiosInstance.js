95cef51e215005a982388d234caa189ccfcf8027
Gaurav <gaurav.baranawal@sparxitsolutions.com>
2025-04-05 11:56:35 +0530

    added api path

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -3,13 +3,14 @@ import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 export const imgPath = "http://localhost:4001/";
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
 export const apipath =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000/api/"
     : process.env.NODE_ENV === "staging"
     ? "https://makapt-api.vercel.app/api/"
-    : "/api/";
+    : "https://makapt-api.vercel.app/api/";
 const axiosInstance = axios.create({
   baseURL: apipath,
 });
